fa4f386428dc76f7bfbdffa60f46edab4b9a6d95
furutani <furutani@est.co.jp>
2022-07-21 13:36:56 +0900

    test

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled";
+import { ErrorMessage } from "@hookform/error-message";
 import { Button, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
 import Paper from '@mui/material/Paper';
 import { Box, Container, createTheme, ThemeProvider } from "@mui/system";
-import React, {FC, ReactElement, useState} from "react";
+import React, {useState} from "react";
 import { useForm } from 'react-hook-form';
 
 import GetAll from "./GetAll";
@@ -10,50 +11,24 @@ import GetId from "./GetId";
 import Post from "./Post";
 import Put from "./Put";
 import Delete from "./Delete";
-import { type } from "@testing-library/user-event/dist/type";
 
-/* type Props = {
-  num?: number
-} */
-
-//const [select, setSelect] = useState<number>();
-
-/* const ComponentChange = (prop: number): ReactElement => {
-    //const num2: number = num;
-    if(prop === 0) {
+const ComponentChange = (props) => {
+    const num = props.num;
+    if(num == 0) {
         return <GetAll />
-    } else if(props.num === 1) {
+    } else if(num == 1) {
         return <GetId />
-    } else if(props.num === 2) {
+    } else if(num == 2) {
         return <Post />
-    } else if(props.num === 3) {
+    } else if(num == 3) {
         return <Put />
-    } else if(props.num === 4) {
+    } else if(num == 4) {
         return <Delete />
-    } else {
-      return <div>0</div>
     }
-} */
+}
 
 const Main = () => {
-    const [componentState, setComponentState] = useState<number>();
-
-    const ComponentChange = (num?: number): ReactElement => {
-      //const num2: number = num;
-      if(num === 0) {
-          return <GetAll />
-      } else if(num === 1) {
-          return <GetId />
-      } else if(num === 2) {
-          return <Post />
-      } else if(num === 3) {
-          return <Put />
-      } else if(num === 4) {
-          return <Delete />
-      } else {
-        return <div>0</div>
-      }
-  }
+    const [componentState, setComponentState] = useState([])
 
     return (
         <Box>
@@ -84,9 +59,7 @@ const Main = () => {
                     top: 50
                 }}
             >
-                {/* <div><ComponentChange prop={componentState}/></div> */}
-
-                {ComponentChange(componentState)}
+                <div><ComponentChange num={componentState} /></div>
             </Box>
         </Box>
     )
